b5cba7b7f4f99b6565e034079b9858ea803de95a
ppae <andeppsc@gmail.com>
2019-03-18 00:33:41 +0200

    -

--- main.hs
+++ main.hs
@@ -16,7 +16,6 @@ get_message h = forever $ do
                 let m = Ping { m_msg = "swap dis naw" , m_trace = "|", m_dest = (rem (h_rate h) 5)}
                 yield m
 
-
 print_message = forever $ do
                 delayThread 1
                 m <- await
@@ -27,13 +26,15 @@ sign_message id sym = forever $ do
                         m <- await
                         yield $ m { m_trace = (m_trace m) ++ "->" ++ sym ++ show id }
 
+send_message out m = lift $ ignore_m $ atomically $ send out m
+
+
 
 
 h_service h input out_node = do
                                 send_task <- async $ h_send_msg h out_node
                                 recv_task <- async $ h_recv_msg h input
                                 wait send_task
-
 
 h_send_msg h out_node = runEffect $ get_message h
                         >-> sign_message (h_id h) symHuman
@@ -46,44 +47,72 @@ h_recv_msg h input = runEffect $ fromInput input
 
 
 
-r_service r input out_nodes = runEffect $ fromInput input
+r_service r input out_nodes = runEffect $ broadcast_message r out_nodes
+                                >-> fromInput input
                                 >-> sign_message (r_id r) symRouter
-                                >-> r_route (r_table r) out_nodes
+                                >-> r_route r out_nodes
 
 
-r_route table out_nodes = forever $ do
+broadcast_message r out_nodes = do
+                let m = Routing {n_table = r_table r, n_source = r_id r}
+                    outs =  get_nodes out_nodes (r_outs r) (r_table r)
+                send_out outs m
+
+
+r_route r out_nodes = forever $ do
                             m <- await
                             let
-                               (_, out_id, table) = r_compute m table
-                               -- out = get_node out_nodes out_id
-                               out = get_node out_nodes $ table !! m_dest m
-                            lift $ ignore_m $ atomically $ send out m
+                               (m', outs_ids, table) = r_compute m r
+                               r = r { r_table = table }
+                               outs =  get_nodes out_nodes outs_ids (r_table r)
+                            send_out outs m'
 
-r_compute :: Message -> [(Int, Int)] -> (Message, (Int, Int), [(Int, Int)])
+get_nodes nodes ids table = map ( (get_node nodes) . fst . (table !!) ) ids
+get_node nodes id = snd $ head $ filter (\(id, pipe) -> id==id) nodes
+
+
+send_out [] m = do return ()
+send_out out m = do
+                    send_message (head out) m
+                    send_out (init out) m
+
+
+-- r_compute :: Message -> [(Int, Int)] -> (Message, (Int, Int), [(Int, Int)])
 r_compute Ping { m_msg = msg
                , m_trace = trace
                , m_dest = dest
                 }
-                table = (Ping { m_msg = msg
+                r = (Ping { m_msg = msg
                               , m_trace = trace
                               , m_dest = dest
                               }
-                        , table !! dest
-                        , table)
-
-r_compute Routing { n_table = new_table
-                  , n_source = s
+                        , [dest]
+                        , r_table r)
+r_compute Routing { n_table = table
+                  , n_source = source
                   }
-                table = (Routing { n_table = improve_table new_table table
-                                 , n_source = s
-                                 }
-                        , table !! 1
-                        , table)
+                r = (Routing { n_table = new_table
+                               , n_source = r_id r
+                             }
+                        , r_outs r
+                        , new_table)
+                 where
+                     new_table = improve_table source table (r_table r)
 
--- improve_table new_table table = map (\((n1,d1), (n1,d1)) -> )zip new_table table
-improve_table new_table table = table
 
-get_node nodes id = snd $ head $ filter (\(id, pipe) -> id==id) nodes
+improve_table source t table = zipWith return_min_dist t' table
+                                where
+                                    t' = map (\(n, d) -> (source, d+1)) t
+
+return_min_dist :: (Int, Int) -> (Int, Int) -> (Int, Int)
+return_min_dist (n1,d1) (n2,d2)
+                                | d1 < d2   = (n1, d1)
+                                | otherwise   = (n2, d2)
+
+-- improve_table new_table table = table
+
+
+
 
 
 
@@ -108,13 +137,10 @@ main = do
     pipes <- sequence $ map (\_ -> spawn unbounded) [0..(num-1)]
 
     let
-        -- ff = zip ids ((map (\a -> [a]) $ map h_out humans) ++ (map r_outs routers) )
-
         h_readers = [ reader $ pipes !! i | i <- h_ids ]
         h_writers = [ writer $ pipes !! (h_out h) | h <- humans ]
 
         r_readers = [ reader $ pipes !! i | i <- r_ids ]
-        -- r_writers = [ [ writer $ pipes !! i | i <- outs ] | outs <- (map r_outs routers) ]
         r_writers = [ [ (i, writer $ pipes !! i) | i <- outs ] | outs <- (map r_outs routers) ]
 
     print $ ""
@@ -127,6 +153,5 @@ main = do
     r_tasks <- sequence $ [async $ task | task <- zipWith3 r_service routers r_readers r_writers ]
 
     waitAny h_tasks
-    -- return ()
 
 
